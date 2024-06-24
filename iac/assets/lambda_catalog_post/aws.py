8a8790033b1e293a22d1feedef81c4c53bfa2ba8
itrullenque <itrullenques@gmail.com>
2024-06-24 10:03:21 +0200

    lambda_post creation

---
+++ iac/assets/lambda_catalog_post/aws.py
@@ -0,0 +1,51 @@
+import decimal
+import json
+import os
+import sys
+
+import boto3
+from boto3.dynamodb.conditions import Attr, Key
+from botocore.config import Config
+from botocore.exceptions import ClientError, ParamValidationError
+
+
+class DynamoDb:
+
+    @staticmethod
+    def resource_custom():
+        my_config = Config(region_name="us-east-1", connect_timeout=3, read_timeout=3)
+        resource = boto3.resource("dynamodb", config=my_config)
+        return resource
+
+    @staticmethod
+    def object_post(table_name, object, resource):
+
+        try:
+            table = resource.Table(table_name)
+            response = {}
+
+            # Check if the item already exists
+            condition_expression = (
+                Attr("catalog_id").not_exists() & Attr("course_id").not_exists()
+            )
+
+            response = table.put_item(
+                Item=object,
+                ConditionExpression=condition_expression,
+                ReturnValues="NONE",
+            )
+
+            response["code"] = "OK"
+            return response
+
+        except Exception as e:
+            exc_type, exc_obj, exc_tb = sys.exc_info()
+            fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
+            err = "file: {} - line:{} - error: {} - {}".format(
+                fname, exc_tb.tb_lineno, exc_obj, exc_type
+            )
+
+            response["code"] = "NOK"
+            response["error_technical"] = err
+
+        return response
--- iac/assets/lambda_catalog_post/lambda_handler.py
+++ iac/assets/lambda_catalog_post/lambda_handler.py
@@ -1,6 +1,34 @@
-def lambda_handler(event, context):
-    # Process the event here
-    print("Event received:", event)
+import boto3
+import json
+import time
+from io import BytesIO
+from aws import DynamoDb
 
-    # Return a response
-    return {"statusCode": 200, "body": "Hello from Lambda!"}
+TABLE_NAME = "catalog"
+resource_dynamo_db = DynamoDb.resource_custom()
+
+
+def lambda_handler(event, context):
+
+    try:
+        catalog_id = event["catalog_id"]
+        course_id = event["course_id"]
+
+        # create the body
+        catalog_item = {
+            "catalog_id": catalog_id,
+            "course_id": course_id,
+            "creation_date": int(time.time()),
+        }
+
+        # save in dynamo
+        dynamo_response = DynamoDb.object_post(
+            resource_dynamo_db, TABLE_NAME, catalog_item
+        )
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps("Catalog item created successfully"),
+        }
+    except Exception as e:
+        return {"statusCode": 500, "body": json.dumps(f"Error: {str(e)}")}
