ea4e434f121097e0d26f0ecc5109c4d16c4543c4
itrullenque <itrullenques@gmail.com>
2024-06-24 10:56:01 +0200

    lambda_post modifications

--- iac/assets/lambda_catalog_post/lambda_handler.py
+++ iac/assets/lambda_catalog_post/lambda_handler.py
@@ -11,8 +11,20 @@ resource_dynamo_db = DynamoDb.resource_custom()
 def lambda_handler(event, context):
 
     try:
-        catalog_id = event["catalog_id"]
-        course_id = event["course_id"]
+        print(f"Received event: {event}")
+        query_params = event.get("queryStringParameters", {})
+
+        # validation
+        if set(query_params.keys()) != {"catalog_id", "course_id"}:
+            return {
+                "statusCode": 400,
+                "body": json.dumps(
+                    "Error: Error, input parameters are missing or incorrect."
+                ),
+            }
+
+        catalog_id = query_params.get("catalog_id")
+        course_id = query_params.get("course_id")
 
         # create the body
         catalog_item = {
@@ -25,6 +37,18 @@ def lambda_handler(event, context):
         dynamo_response = DynamoDb.object_post(
             resource_dynamo_db, TABLE_NAME, catalog_item
         )
+
+        if dynamo_response["code"] == "NOK":
+            return {
+                "statusCode": 500,
+                "body": json.dumps(f"Error: {dynamo_response['error_technical']}"),
+            }
+
+        if dynamo_response["code"] == "CEX":
+            return {
+                "statusCode": 400,
+                "body": json.dumps(f"Error: {dynamo_response['error_technical']}"),
+            }
 
         return {
             "statusCode": 200,
